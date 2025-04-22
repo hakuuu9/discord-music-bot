dea77ef6237573a2a455f76f14a4f273b0fad6a7
hakuuu9 <hakuunlyy@gmail.com>
2025-04-22 22:09:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-apt-get update && apt-get install -y ffmpeg
+
+# Download static ffmpeg binary
+mkdir -p ffmpeg
+cd ffmpeg
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz --strip-components=1
+chmod +x ffmpeg
+
+# Move back to project root
+cd ..
